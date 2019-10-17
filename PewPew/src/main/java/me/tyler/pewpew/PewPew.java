2b0e2ece89c85240660411b6396f2746926db140
Tom_The_Geek <tgbryan39@gmail.com>
2019-10-17 10:40:16 +0100

    Upgraded to 1.14.4 and made classes fit more conventions

--- PewPew/src/main/java/me/tyler/pewpew/PewPew.java
+++ PewPew/src/main/java/me/tyler/pewpew/PewPew.java
@@ -1,9 +1,15 @@
 package me.tyler.pewpew;
 
-import me.tyler.pewpew.Events.BowPew;
+import me.tyler.pewpew.events.BowPew;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PewPew extends JavaPlugin {
+
+    private String bowPewTitle;
+
+    public String getBowPewTitle() {
+        return this.bowPewTitle;
+    }
 
     @Override
     public void onEnable() {
@@ -17,9 +23,12 @@ public final class PewPew extends JavaPlugin {
         
         getConfig().options().copyDefaults();
         saveDefaultConfig();
-        getServer().getPluginManager().registerEvents(new BowPew(), this);
-        System.out.println("\u001B[2;29;93m[\u001B[m\u001B[2;49;34mPewPew\u001B[m\u001B[2;29;93m]\u001B[m \u001B[2;29;93m-\u001B[m \u001B[2;49;92mEnabled\u001B[m");
 
+        this.bowPewTitle = this.getConfig().getString("Bow-Used");
+
+        getServer().getPluginManager().registerEvents(new BowPew(this.bowPewTitle), this);
+
+        System.out.println("\u001B[2;29;93m[\u001B[m\u001B[2;49;34mPewPew\u001B[m\u001B[2;29;93m]\u001B[m \u001B[2;29;93m-\u001B[m \u001B[2;49;92mEnabled\u001B[m");
     }
 
     @Override
